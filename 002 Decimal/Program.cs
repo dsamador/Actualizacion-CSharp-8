287c04f0c08e34764bae806c3809f08eddf72b27
David Amador <54488581+dsamador@users.noreply.github.com>
2022-07-05 00:08:56 -0500

    Decimal

---
+++ 002 Decimal/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+float a = 1f;
+float b = 0.1f;
+
+float r = a - (b * 10);
+Console.WriteLine($"Con flotante {r}");
+
+decimal a1 = 1;
+decimal b1 = 0.1m;
+
+decimal r1 = a1 - (b1 * 10);
+Console.WriteLine($"Con decimal {r1}");
