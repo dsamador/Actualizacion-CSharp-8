e060eb9ecf8078091a33fbd6539883e2dae7adc2
David Amador <54488581+dsamador@users.noreply.github.com>
2022-07-04 23:55:35 -0500

    001 Overflow

---
+++ 001 Overflow/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int n = 1000000;
+int m = 1000000;
+
+//Genera overflow, pero no nos enteramos
+int r = n  * m;
+Console.WriteLine(r);
+Console.WriteLine("-------");
+
+//Recibimos notificacion del overflow
+//int r2 = checked(n * m);
+//Console.WriteLine(r2);
+//Console.WriteLine("-------");
+
+//Ahora capturamos la excepcion
+int r3 = 0;
+int r4 = 0;
+
+try
+{
+    //podemos tener un bloque donde se
+    //verifican todas la operaciones que
+    //causan overflow
+    checked
+    {
+        r3 = n * m;
+        r4 = n + m;
+    }
+}
+catch 
+{
+
+    Console.WriteLine("Problema de overflow");
+}
