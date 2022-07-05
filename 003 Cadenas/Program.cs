f8555d1196860fee764966adaaf57b08d7725ed7
David Amador <54488581+dsamador@users.noreply.github.com>
2022-07-05 13:50:35 -0500

    Cadenas

---
+++ 003 Cadenas/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Como usar caracteres unicode codificados como 4
+//digitos hexadecimales
+char c = '\u00A9';
+Console.WriteLine($"{c}");
+
+//Concatenacion con valores numericos
+//ToString se invoca automaticamente
+
+Console.WriteLine("El cinco es " + 5);
+
+float a = 3.567891f;
+
+Console.WriteLine("El valor de a " + a);
+
+//Interpolacion de cadena
+Console.WriteLine($"El {c} valor de {a} es importante");
