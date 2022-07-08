56cbe61477532ee7eb435c0b3363f106d76537ca
David Amador <54488581+dsamador@users.noreply.github.com>
2022-07-08 14:17:10 -0500

    Usos de this, empezando

---
+++ 007 Uso de this/Automovil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Uso_de_this
+{
+    internal class Automovil
+    {
+        private string modelo = "Ninguno asignado";
+
+        //Primer uso
+        //Usamos this para quitar la ambiguedad entre el nombre del 
+        //parametro y el atributo
+        public Automovil(string modelo)
+        {
+            //Aqui aparece una ambiguedad para quien lea el codigo
+            //la computadora hace referencia al parametro del constructor
+            //modelo = "4";
+
+            //Ahora indicamos que usamos el atributo
+            //this es una autorreferencia al objeto que esta ejecutando el codigo
+            this.modelo = "Corsa";            
+        }
+        public override string ToString()        
+            => string.Format($"Tu auto es {modelo}");//4:12
+        
+    }
+}
